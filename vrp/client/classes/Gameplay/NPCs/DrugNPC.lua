3bbb8ce68c035a59f6213d15bbdbdf8e613c73a5
Nils <socialmedia_flex@web.de>
2023-06-27 13:55:49 +0200

    add basics

---
+++ vrp/client/classes/Gameplay/NPCs/DrugNPC.lua
@@ -0,0 +1,11 @@
+-- ****************************************************************************
+-- *
+-- *  PROJECT:     vRoleplay
+-- *  FILE:        client/classes/Gameplay/NPCs/DrugNPC.lua
+-- *  PURPOSE:     Drug NPC class (ped for selling drugs.)
+-- *
+-- ****************************************************************************
+DrugNPC = inherit(Singleton)
+
+function DrugNPC:constructor()
+end
