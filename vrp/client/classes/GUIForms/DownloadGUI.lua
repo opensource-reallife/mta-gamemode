77c889f4629ec547d69508434f7130934eed2eb2
Amalrix <16260392+Amalrix@users.noreply.github.com>
2023-11-30 01:36:51 +0100

    Added login music to DownloadGUI

--- vrp/client/classes/GUIForms/DownloadGUI.lua
+++ vrp/client/classes/GUIForms/DownloadGUI.lua
@@ -9,10 +9,45 @@ function DownloadGUI:constructor()
 	if screenWidth < 1024 then
 		self.m_ResolutionWarning = GUILabel:new(0, screenHeight - 200, screenWidth, 20, "Bitte erhöhe deine Auflösung, um Darstellungsfehler zu vermeiden!", self):setAlignX("center"):setFont(VRPFont(30)):setColor(Color.Red)
 	end
-	self.m_MusicText = GUILabel:new(0, screenHeight - 30, screenWidth, 30, "Drücke 'm', um die Musik zu stoppen!", self):setAlignX("center")
+	if core:get("Login", "LoginMusic", true) then
+		self.m_MusicText = GUILabel:new(0, screenHeight - 30, screenWidth, 30, "Drücke 'm', um die Musik zu stoppen!", self):setAlignX("center")
+	else
+		self.m_MusicText = GUILabel:new(0, screenHeight - 30, screenWidth, 30, "Drücke 'm', um die Musik zu starten!", self):setAlignX("center")
+	end
 	self.m_ProgressBar = GUIProgressBar:new(screenWidth/2 - 500/2, screenHeight/2 - 150/2 + 110, 500, 30, self)
 
-	fadeCamera(false)
+	self:launchMusic()
+end
+
+function DownloadGUI:launchMusic()
+	if not self:isVisible() then return end
+	if core:get("Login", "LoginMusic", true) then
+		self.m_Music = playSound(INGAME_WEB_PATH .. "/ingame/DownloadMusic.mp3", true)
+		self.m_Music:setVolume(0.3)
+	end
+	self.m_StopMusicFunc = function()
+		if self.m_Music then
+			destroyElement(self.m_Music)
+			self.m_Music = nil
+			self.m_MusicText:setText("Drücke 'm', um die Musik zu starten!")
+			core:set("Login", "LoginMusic", false)
+			self:bind("m", self.m_StartMusicFunc)
+		end
+	end
+	self.m_StartMusicFunc = function()
+		if not self.m_Music then
+			self.m_Music = playSound(INGAME_WEB_PATH .. "/ingame/DownloadMusic.mp3", true)
+			self.m_Music:setVolume(0.3)
+			self.m_MusicText:setText("Drücke 'm', um die Musik zu stoppen!")
+			core:set("Login", "LoginMusic", true)
+			self:bind("m", self.m_StopMusicFunc)
+		end
+	end
+	if core:get("Login", "LoginMusic", true) then
+		self:bind("m", self.m_StopMusicFunc)
+	else
+		self:bind("m", self.m_StartMusicFunc)
+	end
 end
 
 function DownloadGUI:onProgress(p, fullSize)
@@ -33,6 +68,9 @@ end
 
 function DownloadGUI:onComplete()
 	core:ready()
+	if self.m_Music and isElement(self.m_Music) then
+		stopSound(self.m_Music)
+	end
 	fadeCamera(true)
 	self:fadeOut(750)
 	setTimer(
@@ -43,4 +81,5 @@ function DownloadGUI:onComplete()
 			lgi = LoginGUI:new(username, pwhash)
 		end,
 	200, 1)
+	delete(self)
 end
