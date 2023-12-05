94d787f4e7456c1a98d9918fca3dbb7eaef86348
OOPknecht <nein1280@abwesend.de>
2023-12-05 23:51:32 +0100

    test

--- vrp/server/constants/companies.lua
+++ vrp/server/constants/companies.lua
@@ -63,7 +63,7 @@ companySkins[2] = {[44]=true, [50]=true,[192]=true,[268]=true, [309]=true}
 companyDutyMarker[2] = Vector3(2455.18, -2103.97, 13.55)
 companySpawnpoint[2] = {Vector3(2453.93, -2107.24, 13.55), 0, 0}
 
--- Das hier gehört internationalisiert. Test. test2.
+-- Das hier gehört internationalisiert. Test. test2. Test3. Next
 
 -- ID 3 = SAN News:
 companyRankNames[3] = {
