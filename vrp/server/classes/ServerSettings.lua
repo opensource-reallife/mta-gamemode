29e1f7aa2b108e660b76ebab0a46bee17f29db4d
Nils <socialmedia_flex@web.de>
2023-12-31 05:54:35 +0100

    add min members for actions in database

--- vrp/server/classes/ServerSettings.lua
+++ vrp/server/classes/ServerSettings.lua
@@ -21,7 +21,7 @@ function ServerSettings:constructor()
 		end
 
 		if row.Index == "FPSLimit" then
-			setFPSLimit(tonumber(row.Value) or 60)
+			setFPSLimit(tonumber(row.Value) or 65)
 		end
 
 		if row.Index == "ServiceSync" then
@@ -38,6 +38,62 @@ function ServerSettings:constructor()
 
 		if row.Index == "ForumLogin" then
 			FORUM_LOGIN = toboolean(row.Value)
+		end
+
+		if row.Index == "MinMemberWeapontruck" then
+			WEAPONTRUCK_MIN_MEMBERS = DEBUG and 0 or tonumber(row.Value)
+		end
+
+		if row.Index == "MinMemberBankrob" then
+			BANKROB_MIN_MEMBERS = DEBUG and 0 or tonumber(row.Value)
+		end
+
+		if row.Index == "MinMemberCasinoHeist" then
+			CASINOHEIST_MIN_MEMBERS = DEBUG and 0 or tonumber(row.Value)
+		end
+
+		if row.Index == "MinMemberWeedtruck" then
+			WEEDTRUCK_MIN_MEMBERS = DEBUG and 0 or tonumber(row.Value)
+		end
+
+		if row.Index == "MinMemberEvidencetruck" then
+			EVIDENCETRUCK_MIN_MEMBERS = DEBUG and 0 or tonumber(row.Value)
+		end
+
+		if row.Index == "MinMemberArmsdealer" then
+			ARMSDEALER_MIN_MEMBERS = DEBUG and 0 or tonumber(row.Value)
+		end
+
+		if row.Index == "MinMemberShoprob" then
+			SHOPROB_MIN_MEMBERS = DEBUG and 0 or tonumber(row.Value)
+		end
+
+		if row.Index == "MinMemberHouserob" then
+			HOUSEROB_MIN_MEMBERS = DEBUG and 0 or tonumber(row.Value)
+		end
+
+		if row.Index == "MinMemberJewelry" then
+			JEWELRY_MIN_MEMBERS = DEBUG and 0 or tonumber(row.Value)
+		end
+
+		if row.Index == "MinMemberPrisonBreak" then
+			PRISONBREAK_MIN_MEMBERS = DEBUG and 0 or tonumber(row.Value)
+		end
+
+		if row.Index == "MinMemberVehicleShop" then
+			SHOP_VEHICLE_ROB_MIN_MEMBERS = DEBUG and 0 or tonumber(row.Value)
+		end
+
+		if row.Index == "MinMemberChristmastruck" then
+			CHRISTMASTRUCK_MIN_MEMBERS = DEBUG and 0 or tonumber(row.Value)
+		end
+
+		if row.Index == "MinMemberFire" then
+			MIN_PLAYERS_FOR_FIRE = DEBUG and 0 or tonumber(row.Value)
+		end
+
+		if row.Index == "MinMemberVehicleFire" then
+			MIN_PLAYERS_FOR_VEHICLE_FIRE = DEBUG and 0 or tonumber(row.Value)
 		end
 
 		self.m_Settings[row.Index] = row.Value
