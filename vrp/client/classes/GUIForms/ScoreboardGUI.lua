c9b30f9cd50d867cb4cf638b658c18b970cdae1c
Amalrix <16260392+Amalrix@users.noreply.github.com>
2024-01-03 19:00:30 +0100

    Change scoreboard off duty faction color tint

--- vrp/client/classes/GUIForms/ScoreboardGUI.lua
+++ vrp/client/classes/GUIForms/ScoreboardGUI.lua
@@ -217,9 +217,9 @@ function ScoreboardGUI:insertPlayers()
 			if player:getFaction():isInsurgentFaction() then -- Hide Insurgent color (Insurgent = Civilian)
 				item:setColumnColor(3, tocolor(255, 255, 255))
 			elseif not player:getPublicSync("Faction:Duty") then -- Tint faction color when off duty
-				local newR = color.r + (255 - color.r) * 0.7
-				local newG = color.g + (255 - color.g) * 0.7
-				local newB = color.b + (255 - color.b) * 0.7
+				local newR = color.r + (255 - color.r) * 0.85
+				local newG = color.g + (255 - color.g) * 0.85
+				local newB = color.b + (255 - color.b) * 0.85
 				item:setColumnColor(3, tocolor(newR, newG, newB))
 			else
 				item:setColumnColor(3, tocolor(color.r, color.g, color.b))
