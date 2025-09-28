bac97d637d07d0a4758fe249a15a32c657bee8b8
Nils <socialmedia_flex@web.de>
2025-09-28 12:34:24 +0200

    change min wanteds for mostwanted

--- vrp/client/classes/GUIForms/MostWanted3D.lua
+++ vrp/client/classes/GUIForms/MostWanted3D.lua
@@ -26,8 +26,8 @@ function MostWanted:generateUrl()
 	local url = INGAME_WEB_PATH .. "/ingame/other/mostWanted.php?size=1"
 	local i = 1
 	for Id, player in pairs(Element.getAllByType("player")) do
-		if i < 8 then
-			if player:getWanteds() >= 8 then
+		if i < 5 then
+			if player:getWanteds() >= 5 then
 				url = url..("&name[%d]=%s&wanteds[%d]=%d&skin[%d]=%d"):format(i, player:getName(), i, player:getWanteds(), i, player:getModel())
 				i = i+1
 			end
