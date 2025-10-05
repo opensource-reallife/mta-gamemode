aefe08e2195e87aa6bb35e2369ee75a53dc9bb88
Nils <socialmedia_flex@web.de>
2025-10-06 00:20:49 +0200

    fix on last

--- vrp/client/classes/GUIForms/InputBoxWithChanger.lua
+++ vrp/client/classes/GUIForms/InputBoxWithChanger.lua
@@ -8,19 +8,23 @@
 InputBoxWithChanger = inherit(GUIForm)
 
 function InputBoxWithChanger:constructor(title, text, input, items, callback)
-	GUIForm.constructor(self, screenWidth/2 - 707/2, screenHeight/2 - 218/2, 707, 218)
+    GUIWindow.updateGrid()
+	self.m_Width = grid("x", 18)
+	self.m_Height = grid("y", 5)
+
+	GUIForm.constructor(self, screenWidth/2-self.m_Width/2, screenHeight/2-self.m_Height/2, self.m_Width, self.m_Height, true)
 
 	self.m_Window = GUIWindow:new(0, 0, self.m_Width, self.m_Height, title, true, true, self)
-	GUIGridLabel:new(1, 2, 15, 0.5, text, self.m_Window):setHeader(true)
-	self.m_InputBox = GUIGridEdit:new(1, 2.6, 17.5, 1, self.m_Window)
-	self.m_Changer = GUIGridChanger:new(1, 3.6, 17.5, 1.1, self.m_Window)
+	GUIGridLabel:new(1, 1, 15, 1, text, self.m_Window):setFont(VRPFont(30))
+	self.m_InputBox = GUIGridEdit:new(1, 2, 17, 1, self.m_Window)
+	self.m_Changer = GUIGridChanger:new(1, 3, 17, 1, self.m_Window)
 	local item
 	self.m_itemTable = {}
 	for index, v in pairs(items) do
 		item = self.m_Changer:addItem(v)
 		self.m_itemTable[v] = index
 	end
-	self.m_SubmitButton = GUIGridButton:new(1, 5, 7, 1.1, _"Bestätigen", self.m_Window):setBackgroundColor(Color.Green)
+	self.m_SubmitButton = GUIGridButton:new(1, 4, 7, 1, _"Bestätigen", self.m_Window):setBackgroundColor(Color.Green)
 
 	self.m_SubmitButton.onLeftClick = function()
 		if callback then
