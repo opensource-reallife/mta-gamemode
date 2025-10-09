673e0c1f36e121a0ed47a2a933cd4a540a9d98d2
Nils <socialmedia_flex@web.de>
2025-10-09 02:40:17 +0200

    change respawn button tooltip

--- vrp/client/classes/GUIForms/Self/CompanyGUI.lua
+++ vrp/client/classes/GUIForms/Self/CompanyGUI.lua
@@ -40,7 +40,7 @@ function CompanyGUI:constructor()
 	--self.m_CompanyMoneyWithdrawButton = GUIButton:new(self.m_Width*0.56, self.m_Height*0.39, self.m_Width*0.25, self.m_Height*0.07, _"Auszahlen", tabAllgemein):setBarEnabled(true)
 
 	GUILabel:new(self.m_Width*0.02, self.m_Height*0.5, self.m_Width*0.25, self.m_Height*0.1, _"Funktionen:", tabAllgemein)
-	self.m_CompanyRespawnVehicleButton = GUIButton:new(self.m_Width*0.02, self.m_Height*0.6, self.m_Width*0.3, self.m_Height*0.07, _"Fahrzeuge respawnen", tabAllgemein):setBarEnabled(true):setTooltip("Linksklick für sofortigen Respawn \nRechtsklick für angekündigten Respawn", "button", true)
+	self.m_CompanyRespawnVehicleButton = GUIButton:new(self.m_Width*0.02, self.m_Height*0.6, self.m_Width*0.3, self.m_Height*0.07, _"Fahrzeuge respawnen", tabAllgemein):setBarEnabled(true):setTooltip("Linksklick für angekündigten Respawn \nRechtsklick für sofortigen Respawn", "button", true)
 	self.m_CompanyRespawnVehicleButton.onLeftClick = bind(self.CompanyRespawnVehicles, self, true)
 	self.m_CompanyRespawnVehicleButton.onRightClick = bind(self.CompanyRespawnVehicles, self, false)
 
--- vrp/client/classes/GUIForms/Self/FactionGUI.lua
+++ vrp/client/classes/GUIForms/Self/FactionGUI.lua
@@ -43,9 +43,9 @@ function FactionGUI:constructor()
 	--self.m_FactionMoneyAmountEdit = GUIEdit:new(self.m_Width*0.02, self.m_Height*0.39, self.m_Width*0.27, self.m_Height*0.07, tabAllgemein):setCaption(_"Betrag")
 
 	GUILabel:new(self.m_Width*0.02, self.m_Height*0.5, self.m_Width*0.25, self.m_Height*0.1, _"Funktionen:", tabAllgemein)
-	self.m_FactionRespawnVehicleButton = GUIButton:new(self.m_Width*0.02, self.m_Height*0.6, self.m_Width*0.3, self.m_Height*0.07, _"Fahrzeuge respawnen", tabAllgemein):setBarEnabled(true):setTooltip("Linksklick für sofortigen Respawn \nRechtsklick für angekündigten Respawn", "button", true)
-	self.m_FactionRespawnVehicleButton.onLeftClick = bind(self.FactionRespawnVehicles, self, true)
-	self.m_FactionRespawnVehicleButton.onRightClick = bind(self.FactionRespawnVehicles, self, false)
+	self.m_FactionRespawnVehicleButton = GUIButton:new(self.m_Width*0.02, self.m_Height*0.6, self.m_Width*0.3, self.m_Height*0.07, _"Fahrzeuge respawnen", tabAllgemein):setBarEnabled(true):setTooltip("Linksklick für angekündigten Respawn \nRechtsklick für sofortigen Respawn", "button", true)
+	self.m_FactionRespawnVehicleButton.onLeftClick = bind(self.FactionRespawnVehicles, self, false) -- announced
+	self.m_FactionRespawnVehicleButton.onRightClick = bind(self.FactionRespawnVehicles, self, true) -- instant
 
 	self.m_LogButton = GUIButton:new(self.m_Width*0.02, self.m_Height*0.7, self.m_Width*0.3, self.m_Height*0.07, _"Fraktions-Logs", tabAllgemein):setBarEnabled(true)
 	self.m_LogButton.onLeftClick = bind(self.ShowLogs, self)
