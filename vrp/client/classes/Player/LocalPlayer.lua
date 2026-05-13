f7da447f65a78d36daa1e12072f235ed48f63ad8
gitNilszh <kontakt@openreallife.net>
2026-05-13 13:22:25 +0200

    deactivate dcrun, add warning

--- vrp/client/classes/Player/LocalPlayer.lua
+++ vrp/client/classes/Player/LocalPlayer.lua
@@ -813,10 +813,11 @@ function LocalPlayer:Event_setAdmin(player, rank)
 
 		if rank >= ADMIN_RANK_PERMISSION["runString"] then
 			addCommandHandler("dcrun", function(cmd, ...)
-				if self:getRank() >= ADMIN_RANK_PERMISSION["runString"] then
-					local codeString = table.concat({...}, " ")
-					runString(codeString, localPlayer)
-				end
+				WarningBox:new(_"/dcrun ist aufgrund einer AC Violation seitens MTA deaktiviert! Nutze stattdessen /dpcrun")
+				-- if self:getRank() >= ADMIN_RANK_PERMISSION["runString"] then
+				-- 	local codeString = table.concat({...}, " ")
+				-- 	runString(codeString, localPlayer)
+				-- end
 			end)
 		end
 	else
