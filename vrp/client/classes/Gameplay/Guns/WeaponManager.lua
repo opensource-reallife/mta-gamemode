aa36dcb05b7671679dd03994a97d1dc63d4bc986
Amalrix <16260392+Amalrix@users.noreply.github.com>
2026-08-03 16:29:42 +0200

    Fix possible error in weapon manager

--- vrp/client/classes/Gameplay/Guns/WeaponManager.lua
+++ vrp/client/classes/Gameplay/Guns/WeaponManager.lua
@@ -96,10 +96,11 @@ function WeaponManager:isAimingRocketLauncher()
 end
 
 function WeaponManager:isFiringAllowed()
-	if localPlayer:getPublicSync("cuffed") or getElementData(localPlayer, "isTasered") or (NoDm:getSingleton():isInNoDmZone() and not (localPlayer:getFaction():isStateFaction() and localPlayer:getPublicSync("Faction:Duty"))) or 
-		VehicleFuel:isInstantiated() or FishingRod:isInstantiated() or getElementData(localPlayer, "inJail") or 
-		getElementData(localPlayer, "inAdminPrison") or localPlayer.m_IsPickingLock or Damage:getSingleton().m_InTreatment or
-		localPlayer:getData("Admin:IsFrozen") or localPlayer:getPublicSync("ticketsupportMode") then
+	if localPlayer:getPublicSync("cuffed") or getElementData(localPlayer, "isTasered") or (NoDm:getSingleton():isInNoDmZone() and
+	not (localPlayer:getFaction() and localPlayer:getFaction():isStateFaction() and localPlayer:getPublicSync("Faction:Duty"))) or 
+	VehicleFuel:isInstantiated() or FishingRod:isInstantiated() or getElementData(localPlayer, "inJail") or 
+	getElementData(localPlayer, "inAdminPrison") or localPlayer.m_IsPickingLock or Damage:getSingleton().m_InTreatment or
+	localPlayer:getData("Admin:IsFrozen") or localPlayer:getPublicSync("ticketsupportMode") then
 		return false
 	end
 	return true
