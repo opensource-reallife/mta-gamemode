38ac0c1158bfaafdc2f6089b215577dc7b6c6590
Amalrix <16260392+Amalrix@users.noreply.github.com>
2026-08-22 14:50:30 +0200

    Revert "Restore old christmas market"
    This reverts commit 511c8103c84b01beabef5d5282d042938518de4f.

--- vrp/client/classes/Events/Christmas/Christmas.lua
+++ vrp/client/classes/Events/Christmas/Christmas.lua
@@ -13,14 +13,14 @@ function Christmas:constructor()
 
 		--Bubble for Wheel of Fortune
 		local wheelDummy = createElement("d") -- this element is only available serverside as its rotation is synced
-		wheelDummy:setPosition(1479, -1700.3, 14.2)
-		local wheelBubble = SpeakBubble3D:new(wheelDummy, _"Weihnachten", _"Glücksrad", 180, 1.5)
+		wheelDummy:setPosition(1479.24, -1671.78, 14.55)
+		local wheelBubble = SpeakBubble3D:new(wheelDummy, "Weihnachten", "Glücksrad", 180, 1.5)
 		wheelBubble:setBorderColor(Color.LightRed)
 		wheelBubble:setTextColor(Color.LightRed)
 
 
 		--Ferris Wheel ped
-		local ped = Ped.create(189, Vector3(1484.46, -1672.26, 14.05), 149.23)
+		local ped = Ped.create(189, Vector3(1490.94, -1703.03, 14.55), 50)
 		ped:setData("NPC:Immortal", true)
 		ped:setFrozen(true)
 		ped.SpeakBubble = SpeakBubble3D:new(ped, _"Weihnachten", _"Riesenrad", 0, 1.3)
@@ -37,7 +37,7 @@ function Christmas:constructor()
 
 	local ped
 	if EVENT_CHRISTMAS_MARKET then -- spawn the ped at the market
-		ped = Ped.create(221, Vector3(1487.90, -1711.34, 14.05), 90) -- special shop
+		ped = Ped.create(221, Vector3(1456.99, -1684.76, 14.55), 224) -- special shop
 	else -- spawn it near town hall
 		ped = Ped.create(221, Vector3(1456.93, -1748.58, 13.55), 0)
 	end
@@ -56,7 +56,7 @@ function Christmas:constructor()
 
 	local ped
 	if EVENT_CHRISTMAS_MARKET then-- spawn the ped at the market
-		ped = Ped.create(68, Vector3(1468.66, -1706.78, 14.05), -90)-- Quest Ped
+		ped = Ped.create(68, Vector3(1456.50, -1698.16, 14.55), 270)-- Quest Ped
 	elseif DEBUG or getRealTime().monthday <= 24 then -- spawn it near town hall
 		ped = Ped.create(68, Vector3(1452.84, -1745.13, 13.55), 290)
 	end 
